139bd28a17e3beb0a9f38a38236482f243371318
choujohnzyq <choujohnzyq@gmail.com>
2011-02-20 21:52:02 +0000

    Authentification.java

--- GeoHabit/gen/org/inpranet/mobile/R.java
+++ GeoHabit/gen/org/inpranet/mobile/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int button_authentification=0x7f060003;
         public static final int button_login=0x7f060002;
         public static final int edit_login=0x7f060000;
         public static final int edit_password=0x7f060001;
@@ -27,6 +28,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050001;
+        public static final int authentification=0x7f050002;
         public static final int hello=0x7f050000;
     }
 }
