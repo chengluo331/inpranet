f65d07aae08cd76956985dc7d36744f896e2d845
standatw@gmail.com <standatw@gmail.com@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-05 17:50:18 +0000

    deleted GeoHabit.java added tabs in CollectionActivity.java modified Authentification.java moved menu to CollectionActivity.java, service to Authentification.java

---
+++ trunk/GeoHabit/src/com/inpranet/mobile/CollectionActivity.java
@@ -0,0 +1,68 @@
+package com.inpranet.mobile;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TabHost;
+
+public class CollectionActivity extends TabActivity {
+	private static final int INDEX_ACCEUIL = 0;
+	private static final int INDEX_COMMERCE = 1;
+	private static final int INDEX_SPORT = 2;
+	
+	private static final String[] TAB_ID = {"acceuil","commerce","sport"};
+
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.collection);
+		
+		//init tabs et listes
+        TabHost tabHost = getTabHost();
+       
+        tabHost.addTab(tabHost.newTabSpec(TAB_ID[INDEX_ACCEUIL])
+                .setIndicator(getString(R.string.tab_tag_acceuil))
+                .setContent(R.id.listView_acceuil));
+        tabHost.addTab(tabHost.newTabSpec(TAB_ID[INDEX_COMMERCE])
+                .setIndicator(getString(R.string.tab_tag_commerce))
+                .setContent(R.id.listView_commerce));
+        tabHost.addTab(tabHost.newTabSpec(TAB_ID[INDEX_SPORT])
+                .setIndicator(getString(R.string.tab_tag_sport))
+                .setContent(R.id.listView_sport));
+	}
+	
+	/**
+	 *  Afficher le menu décrit dans res.menu.menu.xml lorsque l'on appuie sur boutton menu de l'appareil
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;		
+	}
+	
+	/**
+	 * Procédure appelée lorsque un élement dans le menu est sélectionné
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		
+		// Element settings qui dirige vers l'activité Prefs pour configurer les paramètres
+		case R.id.settings:
+			startActivity(new Intent(this, Prefs.class));
+			break;
+			
+		// Element exit pour quitter l'application
+		case R.id.exit:
+			this.finish();
+			stopService(new Intent(this, LocalizationService.class));
+			break;
+			
+		}
+		return false;
+	}
+}
