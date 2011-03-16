ffe3007bd55570022ec999c672bd32a2373033ff
wenshao.low <wenshao.low@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-16 23:34:54 +0000

    add module habit

---
+++ trunk/HabitXX/src/main/java/com/inpranet/zone/dao/JdbcZoneDao.java
@@ -0,0 +1,29 @@
+package com.inpranet.zone.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+import com.inpranet.zone.model.Zone;
+
+
+@Repository("zoneDao")
+public class JdbcZoneDao extends JdbcDaoSupport implements IZoneDAO {
+
+	public class ZoneRowMapper implements RowMapper {
+
+		public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Zone zone = new Zone(rs.getString("isInside"));
+			return zone;
+		}
+	}
+	
+	public List<Zone>  getZones(int lon, int lat) {
+		String sql = "select geog && ST_GeomFromText('SRID=4326;POINT(4.8768 45.7766)') as isInside from decoupage3";
+		return getJdbcTemplate().query(sql, new ZoneRowMapper());
+	}
+}
