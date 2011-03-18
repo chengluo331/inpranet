512b30795ba5c0166bfce19834d223007ad2406d
standatw <standatw@gmail.com>
2011-03-18 19:08:30 +0000

    delete project HabitXX

--- Mobile/gen/com/inpranet/mobile/R.java
+++ Mobile/gen/com/inpranet/mobile/R.java
@@ -8,48 +8,60 @@
 package com.inpranet.mobile;
 
 public final class R {
+    public static final class array {
+        public static final int entries_precisions=0x7f050000;
+        public static final int entryValues_precisions=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class color {
-        public static final int background=0x7f050000;
+        public static final int background=0x7f060000;
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button_login=0x7f080002;
-        public static final int edit_login=0x7f080000;
-        public static final int edit_password=0x7f080001;
-        public static final int exit=0x7f080008;
-        public static final int imageButton_search=0x7f080003;
-        public static final int listView_acceuil=0x7f080004;
-        public static final int listView_commerce=0x7f080005;
-        public static final int listView_sport=0x7f080006;
-        public static final int settings=0x7f080007;
+        public static final int button_login=0x7f090002;
+        public static final int doc_first_line=0x7f090006;
+        public static final int doc_icon=0x7f090004;
+        public static final int doc_title=0x7f090005;
+        public static final int edit_login=0x7f090000;
+        public static final int edit_password=0x7f090001;
+        public static final int exit=0x7f09000b;
+        public static final int imageButton_search=0x7f090003;
+        public static final int listView_acceuil=0x7f090007;
+        public static final int listView_commerce=0x7f090008;
+        public static final int listView_sport=0x7f090009;
+        public static final int settings=0x7f09000a;
     }
     public static final class layout {
         public static final int authentification=0x7f030000;
         public static final int collection=0x7f030001;
-        public static final int document_lists=0x7f030002;
+        public static final int doc_list_item=0x7f030002;
+        public static final int doc_lists=0x7f030003;
+        public static final int document_lists=0x7f030004;
     }
     public static final class menu {
-        public static final int menu=0x7f070000;
+        public static final int menu=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f060001;
+        public static final int app_name=0x7f070001;
         /**  string utilisé dans main.xml 
          */
-        public static final int authentification=0x7f060002;
-        public static final int hello=0x7f060000;
-        public static final int login=0x7f060003;
-        public static final int menu_exit=0x7f060008;
-        public static final int menu_settings=0x7f060007;
-        public static final int password=0x7f060004;
-        public static final int pref_localizationService_summary=0x7f060006;
-        public static final int pref_localizationService_title=0x7f060005;
-        public static final int tab_tag_acceuil=0x7f060009;
-        public static final int tab_tag_commerce=0x7f06000a;
-        public static final int tab_tag_sport=0x7f06000b;
+        public static final int authentification=0x7f070002;
+        public static final int hello=0x7f070000;
+        public static final int login=0x7f070003;
+        public static final int menu_exit=0x7f070009;
+        public static final int menu_settings=0x7f070008;
+        public static final int password=0x7f070004;
+        public static final int pref_habit_precision_dialog_title=0x7f070007;
+        public static final int pref_habit_precision_summary=0x7f07000b;
+        public static final int pref_habit_precision_title=0x7f07000a;
+        public static final int pref_localizationService_summary=0x7f070006;
+        public static final int pref_localizationService_title=0x7f070005;
+        public static final int tab_tag_acceuil=0x7f07000c;
+        public static final int tab_tag_commerce=0x7f07000d;
+        public static final int tab_tag_sport=0x7f07000e;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
