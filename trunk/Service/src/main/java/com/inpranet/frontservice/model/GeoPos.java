66b22fa54b0971d1a83a7e95f2c80548e7afe032
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-20 14:42:36 +0000

    added zone wsdl

--- trunk/Service/src/main/java/com/inpranet/frontservice/model/GeoPos.java
+++ trunk/Service/src/main/java/com/inpranet/frontservice/model/GeoPos.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement(name="geopos")
 public class GeoPos {
 
 	@XmlElement
