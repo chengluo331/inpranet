3f851b400fb83c3ba1b57d0292bb99b876657b02
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-24 17:58:02 +0000

    replaced postgres by postgis database

--- trunk/DB_admin/Sql/db_init.bat
+++ trunk/DB_admin/Sql/db_init.bat
@@ -2,11 +2,12 @@ REM Fichier pour initialiser la base de donnee
 REM pour que la commande psql puisse etre execute, il faut ajouter le variable d'environnement
 REM dans Path qui reference au repertoire postgreSQL\bin
 set url=localhost
-set db_name=postgres
+set db_name=postgis
 set password=soprasopra
+set username=postgres
 
-psql -h %url% -d %db_name% -U postgres<./creation_schema.sql
-psql -h %url% -d %db_name% -U postgres<./creation_profil.sql
-psql -h %url% -d %db_name% -U postgres<./creation_zone.sql
-psql -h %url% -d %db_name% -U postgres<./creation_document.sql
-psql -h %url% -d %db_name% -U postgres<./creation_habit.sql
+psql -h %url% -d %db_name% -U %username% -W %password%<./creation_schema.sql 
+psql -h %url% -d %db_name% -U %username% -W %password%<./creation_profil.sql
+psql -h %url% -d %db_name% -U %username% -W %password%<./creation_zone.sql
+psql -h %url% -d %db_name% -U %username% -W %password%<./creation_document.sql
+psql -h %url% -d %db_name% -U %username% -W %password%<./creation_habit.sql
