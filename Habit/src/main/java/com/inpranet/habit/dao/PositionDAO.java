5308d299715a68e1f806a0b21586d7f18ebed875
choujohnzyq <choujohnzyq@gmail.com>
2011-03-24 20:32:54 +0000

    implementation StockData part position

--- Habit/src/main/java/com/inpranet/habit/dao/PositionDAO.java
+++ Habit/src/main/java/com/inpranet/habit/dao/PositionDAO.java
@@ -1,5 +1,8 @@
 package com.inpranet.habit.dao;
 
+import java.util.Date;
+import java.util.logging.Logger;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
@@ -7,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.inpranet.habit.model.Position;
+import com.inpranet.habit.service.HabitServiceImp;
 
 /**
  * Implementation de la couche d'acces aux donnees relatives aux positions
@@ -14,14 +18,24 @@ import com.inpranet.habit.model.Position;
  */
 public class PositionDAO implements IPositionDAO{
 
+	/** Logger */
+	static Logger log = Logger.getLogger(HabitServiceImp.class.getName());
+	
 	private JdbcTemplate jdbcTemplate;
-
+	
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 	
 	public void createPosition(Position position) {
 		final String INSERT_POSITION = "INSERT INTO " + TABLE_POSITION + "(user_id, date_time, longitude, latitude) VALUES (?, ?, ?, ?)";
-		jdbcTemplate.update(INSERT_POSITION, new Object[] {position.getUserId(), position.getTime(), position.getLongitude(), position.getLatitude()});
+		try {
+			jdbcTemplate.update(INSERT_POSITION, new Object[] {
+					position.getUserId(), position.getTime(), 
+					position.getLongitude(), position.getLatitude()});
+		} catch (Exception e) {
+			e.printStackTrace();		
+		}
+
 	}
 }
--- Habit/src/main/java/com/inpranet/habit/service/IHabitService.java
+++ Habit/src/main/java/com/inpranet/habit/service/IHabitService.java
@@ -11,15 +11,32 @@ import com.inpranet.core.model.Interest;
 import com.inpranet.core.model.User;
 import com.inpranet.core.model.Zone;
 
-
+/**
+ * Cette interface definit les webservices offerts par le module habit
+ * @author Yiquan
+ *
+ */
 @WebService
 public interface IHabitService {
+	/**
+	 * Sauvegarder un position
+	 * @param user l'objet utilisateur
+	 * @param geoPos l'objet geoPos contenant la coordonnee et la date
+	 * @param zones la liste des zones 
+	 */
 	@WebMethod
 	public void StockData(
 			@WebParam(name="user") User user,
 			@WebParam(name="geoPos") GeoPos geoPos,
 			@WebParam(name="zones") Collection<Zone> zones);
 	
+	/**
+	 * Deduire les zones interessantes pour un utilisateur
+	 * @param user l'objet utilisateur
+	 * @param planningHorizon l'horizon de planification
+	 * @param interests les centres d'interets de l'utilisateur
+	 * @return Collection<Zone> la liste des zones interessantes 
+	 */
 	@WebMethod
 	public Collection<Zone> DeduceZone(
 			@WebParam(name="user") User user, 
