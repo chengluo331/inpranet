56161266c642a01dbc1971a88399fe87ff949c7e
choujohnzyq <choujohnzyq@gmail.com>
2011-03-26 15:32:22 +0000

    table interval and init_interval

--- DB_admin/Sql/creation_habit.sql
+++ DB_admin/Sql/creation_habit.sql
@@ -45,9 +45,59 @@ CREATE TABLE habit.weekly_habit
   zone_id integer NOT NULL,
   nb_occurrence integer NOT NULL,
   flag integer NOT NULL,
-  CONSTRAINT pkey_events PRIMARY KEY (user_id, time_of_week, zone_id)
+  CONSTRAINT pkey_weekly_habit PRIMARY KEY (user_id, time_of_week, zone_id),
+  CONSTRAINT fkey_weekly_habit_interval FOREIGN KEY habit.interval(
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE habit.weekly_habit OWNER TO postgres;
+
+-- Table: habit.interval
+
+-- DROP TABLE habit.interval;
+
+CREATE TABLE habit.interval
+(
+  id integer NOT NULL,
+  day_of_week integer NOT NULL,
+  hour_of_day integer NOT NULL,
+  begin_minute integer NOT NULL,
+  end_minute integer NOT NULL,
+  CONSTRAINT pkey_interval PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE habit.interval OWNER TO postgres;
+
+CREATE OR REPLACE FUNCTION init_interval() RETURNS integer
+AS $$
+DECLARE
+	id integer;
+	day_of_week integer;
+	time_of_day integer;
+BEGIN
+	id := 1;
+  FOR day_of_week IN 1..7 LOOP
+	FOR time_of_day IN 0..23 LOOP
+		INSERT INTO habit.interval
+		VALUES (id, day_of_week, time_of_day, 0, 15);
+		id := id + 1;
+		INSERT INTO habit.interval
+		VALUES (id, day_of_week, time_of_day, 15, 30);
+		id := id + 1;
+		INSERT INTO habit.interval
+		VALUES (id, day_of_week, time_of_day, 30, 45);
+		id := id + 1;
+		INSERT INTO habit.interval
+		VALUES (id, day_of_week, time_of_day, 45, 59);
+		id := id + 1;
+	END LOOP;
+  END LOOP;
+
+  RETURN 1;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT init_interval();
