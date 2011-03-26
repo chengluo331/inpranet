4b5942feb13ea9dc2fa0092c2c8270a1ef6b0a0b
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-26 17:39:55 +0000

    generated sources from wsdl

---
+++ core/src/main/java/com/inpranet/core/ws/habit/ObjectFactory.java
@@ -0,0 +1,164 @@
+
+package com.inpranet.core.ws.habit;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+import com.inpranet.core.model.GeoPos;
+import com.inpranet.core.model.Interest;
+import com.inpranet.core.model.User;
+import com.inpranet.core.model.Zone;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.inpranet.core.ws.habit package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _DeduceZoneResponse_QNAME = new QName("http://service.habit.inpranet.com/", "DeduceZoneResponse");
+    private final static QName _StockDataResponse_QNAME = new QName("http://service.habit.inpranet.com/", "StockDataResponse");
+    private final static QName _DeduceZone_QNAME = new QName("http://service.habit.inpranet.com/", "DeduceZone");
+    private final static QName _Geopos_QNAME = new QName("http://service.habit.inpranet.com/", "geopos");
+    private final static QName _StockData_QNAME = new QName("http://service.habit.inpranet.com/", "StockData");
+    private final static QName _Zone_QNAME = new QName("http://www.inpranet.com/zone", "zone");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.inpranet.core.ws.habit
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link User }
+     * 
+     */
+    public User createUser() {
+        return new User();
+    }
+
+    /**
+     * Create an instance of {@link Interest }
+     * 
+     */
+    public Interest createInterest() {
+        return new Interest();
+    }
+
+    /**
+     * Create an instance of {@link GeoPos }
+     * 
+     */
+    public GeoPos createGeoPos() {
+        return new GeoPos();
+    }
+
+    /**
+     * Create an instance of {@link Zone }
+     * 
+     */
+    public Zone createZone() {
+        return new Zone();
+    }
+
+    /**
+     * Create an instance of {@link DeduceZone }
+     * 
+     */
+    public DeduceZone createDeduceZone() {
+        return new DeduceZone();
+    }
+
+    /**
+     * Create an instance of {@link DeduceZoneResponse }
+     * 
+     */
+    public DeduceZoneResponse createDeduceZoneResponse() {
+        return new DeduceZoneResponse();
+    }
+
+    /**
+     * Create an instance of {@link StockData }
+     * 
+     */
+    public StockData createStockData() {
+        return new StockData();
+    }
+
+    /**
+     * Create an instance of {@link StockDataResponse }
+     * 
+     */
+    public StockDataResponse createStockDataResponse() {
+        return new StockDataResponse();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeduceZoneResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.habit.inpranet.com/", name = "DeduceZoneResponse")
+    public JAXBElement<DeduceZoneResponse> createDeduceZoneResponse(DeduceZoneResponse value) {
+        return new JAXBElement<DeduceZoneResponse>(_DeduceZoneResponse_QNAME, DeduceZoneResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link StockDataResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.habit.inpranet.com/", name = "StockDataResponse")
+    public JAXBElement<StockDataResponse> createStockDataResponse(StockDataResponse value) {
+        return new JAXBElement<StockDataResponse>(_StockDataResponse_QNAME, StockDataResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link DeduceZone }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.habit.inpranet.com/", name = "DeduceZone")
+    public JAXBElement<DeduceZone> createDeduceZone(DeduceZone value) {
+        return new JAXBElement<DeduceZone>(_DeduceZone_QNAME, DeduceZone.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link GeoPos }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.habit.inpranet.com/", name = "geopos")
+    public JAXBElement<GeoPos> createGeopos(GeoPos value) {
+        return new JAXBElement<GeoPos>(_Geopos_QNAME, GeoPos.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link StockData }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.habit.inpranet.com/", name = "StockData")
+    public JAXBElement<StockData> createStockData(StockData value) {
+        return new JAXBElement<StockData>(_StockData_QNAME, StockData.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Zone }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://www.inpranet.com/zone", name = "zone")
+    public JAXBElement<Zone> createZone(Zone value) {
+        return new JAXBElement<Zone>(_Zone_QNAME, Zone.class, null, value);
+    }
+
+}
---
+++ core/src/main/java/com/inpranet/core/ws/habit/StockData.java
@@ -0,0 +1,126 @@
+
+package com.inpranet.core.ws.habit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+import com.inpranet.core.model.GeoPos;
+import com.inpranet.core.model.User;
+import com.inpranet.core.model.Zone;
+
+
+/**
+ * <p>Java class for StockData complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="StockData">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="user" type="{http://service.habit.inpranet.com/}user" minOccurs="0"/>
+ *         &lt;element name="geoPos" type="{http://service.habit.inpranet.com/}geoPos" minOccurs="0"/>
+ *         &lt;element name="zones" type="{http://service.habit.inpranet.com/}zone" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "StockData", propOrder = {
+    "user",
+    "geoPos",
+    "zones"
+})
+public class StockData {
+
+    protected User user;
+    protected GeoPos geoPos;
+    protected List<Zone> zones;
+
+    /**
+     * Gets the value of the user property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link User }
+     *     
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * Sets the value of the user property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link User }
+     *     
+     */
+    public void setUser(User value) {
+        this.user = value;
+    }
+
+    /**
+     * Gets the value of the geoPos property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link GeoPos }
+     *     
+     */
+    public GeoPos getGeoPos() {
+        return geoPos;
+    }
+
+    /**
+     * Sets the value of the geoPos property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link GeoPos }
+     *     
+     */
+    public void setGeoPos(GeoPos value) {
+        this.geoPos = value;
+    }
+
+    /**
+     * Gets the value of the zones property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the zones property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getZones().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Zone }
+     * 
+     * 
+     */
+    public List<Zone> getZones() {
+        if (zones == null) {
+            zones = new ArrayList<Zone>();
+        }
+        return this.zones;
+    }
+
+}
