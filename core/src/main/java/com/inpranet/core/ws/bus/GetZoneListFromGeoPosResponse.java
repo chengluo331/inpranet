934cbc47d43e2f6ce12b398a38e115357d5ce011
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-27 16:55:06 +0000

    fixed bugs in generated files

--- core/src/main/java/com/inpranet/core/ws/bus/GetZoneListFromGeoPosResponse.java
+++ core/src/main/java/com/inpranet/core/ws/bus/GetZoneListFromGeoPosResponse.java
@@ -3,10 +3,13 @@ package com.inpranet.core.ws.bus;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
+
+import com.inpranet.core.model.Zone;
 
 
 /**
