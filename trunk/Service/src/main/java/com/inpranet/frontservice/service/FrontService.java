a76985eb3ac3f5c48ac9694419f504d04bfc89e3
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-27 11:13:33 +0000

    cleaned and commented the code started connecting scenario 2 : get documents from indexation

--- trunk/Service/src/main/java/com/inpranet/frontservice/service/FrontService.java
+++ trunk/Service/src/main/java/com/inpranet/frontservice/service/FrontService.java
@@ -1,104 +1,78 @@
 package com.inpranet.frontservice.service;
 
-import java.util.List;
-import java.util.logging.Level;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.inpranet.core.model.Document;
 import com.inpranet.core.model.GeoPos;
 import com.inpranet.core.model.User;
 import com.inpranet.frontservice.orchestration.BusinessProcessor;
 import com.inpranet.frontservice.orchestration.IBusinessProcessor;
+import com.inpranet.frontservice.orchestration.ServiceException;
+import com.inpranet.frontservice.orchestration.ServiceException.ErrorCode;
 
-@Path("/services/")
+/**
+ * FrontService expose des web services type RestFul
+ * Il sert de "dispatcher" des differentes requetes et utilise un IBusinessProcessor pour orchestrer 
+ * l'ensemble des services sous-jacents necessaires pour repondre a la requete client
+ * Voir {IFrontService} pour plus de details
+ * @author inpranet team
+ *
+ */
 public class FrontService implements IFrontService {
+	
+	/** Logger */
+	static Logger log = Logger.getLogger(FrontService.class.getName());
 
+	/**
+	 * bp est le gestionnaire de processus metier
+	 */
 	private IBusinessProcessor bp = new BusinessProcessor();
 
-	@POST
-	// @Path("/geo/{id}")
-	@Path("/geo")
-	@Consumes({ "application/json", "application/xml" })
-	@Produces({ "application/json", "application/xml" })
-	public void postGeoPos(/* @PathParam("id")String id, */GeoPos pos) {
-
-		Logger.getLogger("FrontService").log(Level.INFO, "CA MARCHE : ");
-
-		User u = new User();
-		u.setIdUser(Integer.parseInt("12"));
-		bp.receiveRawPositions(u, pos);
+	
+	/**
+	 * See interface {IFrontService}
+	 */
+	public void postGeoPos(String token, GeoPos pos) {
+		log.info("Received the request to store a geoposition");
+		try {
+			// recherche l'utilisateur lié au token
+			User user = bp.authenticateUser(token);
+			
+			// lance le processus de reception d'une donne geospatiale
+			bp.receiveRawPositions(user, pos);
+			
+		} catch(ServiceException e) {
+			if (e.getErrorCode() == ErrorCode.BAD_TOKEN) {
+				// TODO: renvoyer une erreur a l'utilisateur
+			}
+		} 
 	}
 
-	/*
-	@GET
-	// @Path("/geo/{id}")
-	@Path("/doc")
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Produces({ MediaType.APPLICATION_JSON })
+	/**
+	 * See interface {IFrontService}
+	 */
 	public Collection<Document> getDocumentList(String id) {
 
+		log.info("Received the request to give back a list of documents");
+		
 		Collection<Document> c = new ArrayList<Document>();
+		/*
 		Document doc1 = new Document(10,"Server test1",true, "acceuil","http://www.test.com",new Date(),new Date(),123,123,"this is a server test");
 		Document doc2 = new Document(11,"Server test2",true, "sport","http://www.test.com",new Date(),new Date(),123,123,"this is a server test");
 		c.add(doc1);
 		c.add(doc2);
+		*/
 		return c;
 	}
-	*/
 
-	/*
-	 * @POST
-	 * 
-	 * @Path("/geo")
-	 * 
-	 * @Consumes({ "application/json", "application/xml" })
-	 * 
-	 * @Produces({ "application/json", "application/xml" }) public void
-	 * serviceGeo(GeoPos pos) {
-	 * 
-	 * Logger.getLogger("FrontService").log(Level.INFO, "CA MARCHE : " +
-	 * pos.getLatitude());
-	 * 
-	 * 
-	 * ClassPathXmlApplicationContext context = new
-	 * ClassPathXmlApplicationContext(new String[] {"inpranet-service.xml"});
-	 * IZoneManager zoneManager = (IZoneManager)context.getBean("serviceZone");
-	 * 
-	 * List<Zone> zones = zoneManager.getZones(2, 3);
-	 * 
-	 * Logger log = Logger.getLogger(this.getClass().getName()); for (Zone zone
-	 * : zones) { log.info(zone.toString()); }
-	 * 
-	 * }
-	 */
-	/*
-	 * @POST
-	 * 
-	 * @Path("/multi/{id}")
-	 * 
-	 * @Consumes({ "application/json", "application/xml" })
-	 * 
-	 * @Produces({ "application/json", "application/xml" }) public void
-	 * testMulti(@PathParam("id")String id, List<GeoPos> pos) { Logger log =
-	 * Logger.getLogger(this.getClass().getName()); log.info(id); log.info("lol"
-	 * + pos.get(0).getLatitude()); log.info("lol" + pos.get(1).getLatitude());
-	 * }
-	 */
-	@POST
-	@Path("/multi")
-	@Consumes({ "application/json", "application/xml" })
-	@Produces({ "application/json", "application/xml" })
-	public List<GeoPos> testMulti(List<GeoPos> pos) {
-		Logger log = Logger.getLogger(this.getClass().getName());
-		// log.info("lol" + pos.get(0).getLatitude());
-		return pos;
-	}
 
 	@GET
 	@Path("/json")
