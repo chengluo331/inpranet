4763a542573450c0959f0cd5491326063f6307d7
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-28 16:09:34 +0000

    ajout de donnees interests correction sur load_zone

---
+++ DB_admin/Sql/load_interests.sql
@@ -0,0 +1,3 @@
+INSERT INTO zone.interest (name, description) VALUES ('Commercial', 'Commercial');
+INSERT INTO zone.interest (name, description) VALUES ('Ville', 'Ville');
+INSERT INTO zone.interest (name, description) VALUES ('Scolaire', 'Scolaire');
