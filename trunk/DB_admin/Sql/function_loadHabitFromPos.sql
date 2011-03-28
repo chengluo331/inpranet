eb52427f7a6c4bbba328366f01798cc0b1c96357
choujohnzyq@gmail.com <choujohnzyq@gmail.com@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-28 14:34:04 +0000

    function_loadHabitFromPos.sql delete two files

--- trunk/DB_admin/Sql/function_loadHabitFromPos.sql
+++ trunk/DB_admin/Sql/function_loadHabitFromPos.sql
@@ -17,26 +17,31 @@ DECLARE
 	t_minute integer;
 
 BEGIN
-
-
 	OPEN c_pos FOR SELECT * FROM habit.position ORDER BY date_time;
-
-	FETCH c_pos INTO rt_pos;
-	-- Recuperer id intervalle
-		SELECT EXTRACT(DOW FROM date_time) INTO t_dow;
-		SELECT EXTRACT(HOUR FROM date_time) INTO t_hour;
-		SELECT EXTRACT(MINUTE FROM date_time) INTO t_minute;
-		SELECT id INTO idInterval FROM habit.interval 
-			WHERE day_of_week=t_dow+1
-			AND hour_of_day=t_hour
-			AND begin_minute<=t_minute
-			AND end_minute>t_minute;
-		-- Recuperer les zones contenant ce point
-		OPEN c_zone FOR SELECT id FROM zone.zone WHERE geog && rt_pos.point;
-		-- Pour chaque zone
-		FETCH c_zone INTO t_idZone;
-			select insertweeklyhabit(rt_pos.user_id, idInterval, t_idZone);
-		CLOSE c_zone;
+	LOOP
+		FETCH c_pos INTO rt_pos;
+			-- Recuperer id intervalle
+			SELECT EXTRACT(DOW FROM date_time) INTO t_dow;
+			SELECT EXTRACT(HOUR FROM date_time) INTO t_hour;
+			SELECT EXTRACT(MINUTE FROM date_time) INTO t_minute;
+			SELECT id INTO idInterval FROM habit.interval 
+				WHERE day_of_week=t_dow+1
+				AND hour_of_day=t_hour
+				AND begin_minute<=t_minute
+				AND end_minute>t_minute;
+				
+			-- Recuperer les zones contenant ce point
+			OPEN c_zone FOR SELECT id FROM zone.zone WHERE geog && rt_pos.point;
+			LOOP
+				-- Pour chaque zone
+				FETCH c_zone INTO t_idZone;
+					select insertweeklyhabit(rt_pos.user_id, idInterval, t_idZone);				
+				EXIT WHEN NOT FOUND;
+			END LOOP;
+			CLOSE c_zone;
+		EXIT WHEN NOT FOUND;
+	END LOOP;
+	CLOSE c_pos;
 	RETURN 1;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
--- trunk/DB_admin/Sql/requete_zone.sql
+++
@@ -1,2 +0,0 @@
-﻿select * from zone 
-where geog && ST_GeomFromText('SRID=4326;POINT(4.8349 45.764)');
