d025fa0ff527375858557dc0177b4dac00acb127
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-28 17:33:13 +0000

    chargement de données test de positions

--- DB_admin/Sql/db_init.bat
+++ DB_admin/Sql/db_init.bat
@@ -6,15 +6,21 @@ set db_name=postgis
 set password=soprasopra
 set username=postgres
 
+# create schemas
 psql -h %url% -d %db_name% -U %username% -w %password%<./creation_schema.sql 
+
+# create tables
 psql -h %url% -d %db_name% -U %username% -w %password%<./creation_profil.sql
 psql -h %url% -d %db_name% -U %username% -w %password%<./creation_zone.sql
 psql -h %url% -d %db_name% -U %username% -w %password%<./creation_document.sql
 psql -h %url% -d %db_name% -U %username% -w %password%<./creation_habit.sql
 
+# create functions
 psql -h %url% -d %db_name% -U %username% -w %password%<./function_insertweeklyhabit.sql
 psql -h %url% -d %db_name% -U %username% -w %password%<./function_loadHabitFromPos.sql
 psql -h %url% -d %db_name% -U %username% -W %password%<./requete_gethabit.sql
 
+# load some data
 psql -h %url% -d %db_name% -U %username% -W %password%<./load_interests.sql
 psql -h %url% -d %db_name% -U %username% -W %password%<./load_zone.sql
+psql -h %url% -d %db_name% -U %username% -W %password%<./load_positions.sql
