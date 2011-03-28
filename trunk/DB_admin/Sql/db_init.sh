85b40dfcfe113d8d230611f89f4a0f9cd8065ebf
yoann.buch <yoann.buch@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-28 17:33:13 +0000

    chargement de données test de positions

--- trunk/DB_admin/Sql/db_init.sh
+++ trunk/DB_admin/Sql/db_init.sh
@@ -1,25 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
 #Fichier pour initialiser la base de donnee
 #pour que la commande psql puisse etre execute, il faut ajouter le variable d'environnement
 #dans Path qui reference au repertoire postgreSQL\bin
 url=localhost
-db_name=postgis
-password=soprasopra
-username=postgres
+db_name='postgis'
+password='soprasopra'
+username='postgres'
 
-echo `psql -h $url -d $db_name -U $username -W $password < ./creation_schema.sql` 
-echo `psql -h $url -d $db_name -U $username -W $password < ./creation_profil.sql`
-echo `psql -h $url -d $db_name -U $username -W $password < ./create_zone.sql`
-echo `psql -h $url -d $db_name -U $username -W $password < ./load_zone.sql`
-echo `psql -h $url -d $db_name -U $username-W $password < ./creation_document.sql`
-echo `psql -h $url -d $db_name -U $username -W $password < ./creation_habit.sql`
+# creation schemas
+psql -h localhost -d postgis -U postgres -W soprasopra < creation_schema.sql
 
+# creation tables
+psql -h localhost -d postgis -U postgres -W soprasopra < creation_profil.sql
+psql -h localhost -d postgis -U postgres -W soprasopra < creation_zone.sql
+psql -h localhost -d postgis -U postgres -W soprasopra < creation_document.sql
+psql -h localhost -d postgis -U postgres -W soprasopra < creation_habit.sql
 
+# fonctions
+psql -h localhost -d postgis -U postgres -W soprasopra < function_insertweeklyhabit.sql
+psql -h localhost -d postgis -U postgres -W soprasopra < function_loadHabitFromPos.sql
+psql -hlocalhost -d postgis -U postgres -W soprasopra < requete_gethabit.sql
 
+# load some data
+psql -hlocalhost -d postgis -U postgres -W soprasopra < load_interests.sql
+psql -hlocalhost -d postgis -U postgres -W soprasopra < load_zone.sql
+psql -hlocalhost -d postgis -U postgres -W soprasopra < load_positions.sql
 
 # doc : http://postgis.refractions.net/docs/ch04.html
 # Chargement du shapefile dans la bonne table
-# shp2pgsql -s 4326 -c -D -G -I -W "utf-8" zone.shp zone.zone > load_zone.sql
+# /opt/PostgreSQL/9.0/bin/shp2pgsql.bin -s 4326 -c -D -G -I -W "utf-8" zone.shp zone.zone > load_zone.sql
 # psql -d roadsdb -f roads.sql
 #
+
+# psql -h localhost -d postgis -U postgres -w soprasopra < creation_schema.sql
