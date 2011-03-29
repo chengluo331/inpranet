0c6f8c2189d9d26be673d5019f883478513cfa84
choujohnzyq@gmail.com <choujohnzyq@gmail.com@1cb6e9aa-dba8-0c1a-6532-8c941eba687e>
2011-03-29 17:20:38 +0000

    Remise a 0 les flags + commentaires

--- trunk/DB_admin/Sql/function_loadHabitFromPos.sql
+++ trunk/DB_admin/Sql/function_loadHabitFromPos.sql
@@ -1,27 +1,45 @@
 -- Function: habit.loadHabitFromPos()
--- DROP FUNCTION habit.loadHabitFromPos();
+/* La fonction permet de creer des donnees habitudes dans la table
+	habit ¨¤ partir des donn¨¦es positions
+*/
 
+-- DROP FUNCTION habit.loadHabitFromPos();
 CREATE OR REPLACE FUNCTION habit.loadHabitFromPos()
  RETURNS integer  AS
 $BODY$
 DECLARE
-        c_pos REFCURSOR;
-        rt_pos habit.position%ROWTYPE;
+        c_pos REFCURSOR; -- Curseur position
+        rt_pos habit.position%ROWTYPE; 
         
-        c_zone REFCURSOR;
+        c_zone REFCURSOR; -- Curseur zone
         t_idZone zone.zone.id%TYPE;
         
-        idInterval integer;
-        t_dow integer;
-        t_hour integer;
-        t_minute integer;
+        idInterval integer; -- Identifiant d'intervalle de la date
+        t_dow integer; -- le jour de la semaine
+        t_hour integer; -- l'heure du jour
+        t_minute integer; -- la minute
+		t_week integer; -- la semaine de l'ann¨¦e
+		current_week integer; -- la semaine courante du traitement
 
 BEGIN
+		current_week:=0;
+		-- R¨¦cup¨¦rer tous les enregistrements dans la table position
         OPEN c_pos FOR SELECT * FROM habit.position ORDER BY date_time;
         LOOP
+			-- Pour chaque position
                 FETCH c_pos INTO rt_pos;
                 EXIT WHEN NOT FOUND;
-                        -- Recuperer id intervalle
+						
+						-- Recuperer la semaine de la date
+						SELECT EXTRACT(WEEK FROM rt_pos.date_time) INTO t_week;
+						-- Si c'est une nouvelle semaine differente que la semaine courante,
+						-- Remise a zero de tous les flags
+						IF t_week <> current_week THEN
+							UPDATE habit.weekly_habit SET flag=0;
+							current_week:=t_week;
+						END IF;
+						
+                        -- Recuperer id intervalle						
                         SELECT EXTRACT(DOW FROM rt_pos.date_time) INTO t_dow;
                         SELECT EXTRACT(HOUR FROM rt_pos.date_time) INTO t_hour;
                         SELECT EXTRACT(MINUTE FROM rt_pos.date_time) INTO t_minute;
@@ -37,10 +55,10 @@ BEGIN
                                 -- Pour chaque zone
                                 FETCH c_zone INTO t_idZone;
                                 EXIT WHEN NOT FOUND;
+										-- Appel ¨¤ la proc¨¦dure stock¨¦e insertweeklyhabit
                                         PERFORM habit.insertweeklyhabit(rt_pos.user_id, idInterval, t_idZone);                                                  
                         END LOOP;
-                        CLOSE c_zone;
-                
+                        CLOSE c_zone;                
         END LOOP;
         CLOSE c_pos;
         RETURN 1;
