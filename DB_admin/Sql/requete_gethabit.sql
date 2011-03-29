edb98b3536021d6cdca5e5b44900d57b8da457b3
wenshao.low <wenshao.low@gmail.com>
2011-03-29 10:59:40 +0000

    update gethabit.. now we can get zone!!!!! ;)

--- DB_admin/Sql/requete_gethabit.sql
+++ DB_admin/Sql/requete_gethabit.sql
@@ -14,18 +14,20 @@ CREATE OR REPLACE FUNCTION habit.gethabit(uid integer, idinter integer, pday int
 $BODY$
 DECLARE
 	maxbuffer integer; --stocker la valeur de retour de requete
+	zonebuffer integer;
 	rtuser profil.habit_choice%rowtype;
 	today_daily boolean; --variable de verification de choix utilisateur concernant le temps requetee
 	
 BEGIN
 	--recuperer la valeur du choix du jour
-	-- si c'est un choix habitude quotidien, on recupere le max de somme correspondant au nbr_occurrence sur un centre interet donné
-	-- sinon, si c'est un choix habitude hebdomadaire, on recupere le max correspondant au nbr_occurrence sur un centre interet donné
+	-- si c'est un choix habitude quotidien, on recupere la zone ou se trouve l'utilisateur quotidiennement 
+	-- (ne prend pas en compte les habitudes hebdomadaire) pour un centre interet donné
+	-- sinon, si c'est un choix habitude hebdomadaire, on recupere la zone d'un centre interet donné ou se trouve l'utilisateur le jour de la semaine
 	SELECT daily INTO today_daily FROM profil.habit_choice WHERE user_id=uid AND day_of_week=pday;
 
 	IF today_daily = TRUE THEN 
-	SELECT MAX(sum) INTO maxbuffer FROM (
-			SELECT SUM(nb_occurrence) AS sum FROM habit.weekly_habit, zone.zone, habit.interval, profil.habit_choice
+	SELECT MAX(sum), idzone INTO maxbuffer, zonebuffer FROM (
+			SELECT SUM(nb_occurrence) AS sum, zone_id AS idzone FROM habit.weekly_habit, zone.zone, habit.interval, profil.habit_choice
 				WHERE weekly_habit.zone_id=zone.id 
 					AND weekly_habit.time_of_week=interval.id
 					AND weekly_habit.user_id=habit_choice.user_id
@@ -36,9 +38,11 @@ BEGIN
 					AND hour_of_day=phour
 					AND pminutes between begin_minute and end_minute
 					GROUP BY zone_id)
-					AS sumbuffer;
+					AS sumbuffer
+					GROUP BY idzone
+					ORDER BY max DESC;
 	ELSE
-		SELECT MAX(nb_occurrence) INTO maxbuffer FROM habit.weekly_habit, zone.zone, habit.interval, profil.habit_choice
+		SELECT MAX(nb_occurrence), zone_id INTO maxbuffer, zonebuffer FROM habit.weekly_habit, zone.zone, habit.interval, profil.habit_choice
 			WHERE weekly_habit.zone_id=zone.id 
 				AND weekly_habit.time_of_week=interval.id
 				AND weekly_habit.user_id=habit_choice.user_id
@@ -47,10 +51,12 @@ BEGIN
 				AND interest_id=idinter
 				AND interval.day_of_week=pday
 				AND hour_of_day=phour
-				AND pminutes between begin_minute and end_minute;
+				AND pminutes between begin_minute and end_minute
+			GROUP BY zone_id
+			ORDER BY max DESC;
 	END IF;
 
-return maxbuffer;
+return zonebuffer;
 
 
 END;$BODY$
